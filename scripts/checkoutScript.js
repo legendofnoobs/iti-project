63828622e3ade93ca72ecacaf9491512c337e93d
moshams272 <mohamedshamseldeen95@gmail.com>
2025-08-15 11:58:56 +0300

    handle submit

--- scripts/checkoutScript.js
+++ scripts/checkoutScript.js
@@ -1,12 +1,12 @@
-function createErrorMessageAndFocus(element,message){
-    if (!element.nextElementSibling) {
-      const errorMessege = document.createElement("p");
-      errorMessege.innerText = message;
-      errorMessege.classList.add("error-message");
-      element.parentNode.appendChild(errorMessege);
-    }
-    element.scrollIntoView({ behavior: "smooth" });
-    element.focus();
+function createErrorMessageAndFocus(element, message) {
+  if (!element.nextElementSibling) {
+    const errorMessege = document.createElement("p");
+    errorMessege.innerText = message;
+    errorMessege.classList.add("error-message");
+    element.parentNode.appendChild(errorMessege);
+  }
+  element.scrollIntoView({ behavior: "smooth" });
+  element.focus();
 }
 
 function handleSubmit(event) {
@@ -18,67 +18,99 @@ function handleSubmit(event) {
   const town = document.querySelector(`input[name="billing-form-town"]`);
   const phone = document.querySelector(`input[name="billing-form-phone"]`);
   const email = document.querySelector(`input[name="billing-form-email"]`);
-  
-  if (!(/^[a-zA-Z\s-]+$/).test(fullName.value.trim())) {
-    createErrorMessageAndFocus(fullName,"Please enter character from a to z or from A to Z !");
+
+  if (!/^[a-zA-Z\s-]+$/.test(fullName.value.trim())) {
+    createErrorMessageAndFocus(
+      fullName,
+      "Please enter character from a to z or from A to Z !"
+    );
     return;
   }
 
   if (!streetAddress.value.trim()) {
-    createErrorMessageAndFocus(streetAddress,"Please enter the street address !");
+    createErrorMessageAndFocus(
+      streetAddress,
+      "Please enter the street address !"
+    );
     return;
   }
 
   if (!town.value.trim()) {
-    createErrorMessageAndFocus(town,"Please enter the town !");
+    createErrorMessageAndFocus(town, "Please enter the town !");
     return;
   }
 
-  if (!(/^[0-9]{11}$/).test(phone.value.trim())) {
-    createErrorMessageAndFocus(phone,"Please enter the phone with 11 numbers only !");
+  if (!/^[0-9]{11}$/.test(phone.value.trim())) {
+    createErrorMessageAndFocus(
+      phone,
+      "Please enter the phone with 11 numbers only !"
+    );
     return;
   }
 
-  if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(email.value.trim())) {
-    createErrorMessageAndFocus(email,"Please enter email as lol@gmail.com");
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value.trim())) {
+    createErrorMessageAndFocus(email, "Please enter email as lol@gmail.com");
     return;
   }
 
-  showPopup();
-
-  setTimeout(()=>{
-    window.location.href="../index.html";
-  },3000);
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ fullName, streetAddress, town, phone, email }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    })
+    .then(() => {
+      showPopup();
+      event.target.reset();
+      setTimeout(() => {
+        window.location.href = "../index.html";
+      }, 3000);
+    })
+    .catch(() =>{
+      const placeOrderButton = document.getElementById("place-order-btn");
+      createErrorMessageAndFocus(placeOrderButton,"Try again later, Please :(")
+    });
 }
 
-function clearErrorMessage(element){
-    element.nextElementSibling?.remove();
+function clearErrorMessage(element) {
+  element.nextElementSibling?.remove();
 }
 
 function showPopup() {
-  const popup=document.createElement("div");
-  popup.id="popup";
-  const popupContent=document.createElement("p");
-  popupContent.innerText="Order placed successfully 🙌";
+  const popup = document.createElement("div");
+  popup.id = "popup";
+  const popupContent = document.createElement("p");
+  popupContent.innerText = "Order placed successfully 🙌";
   popupContent.classList.add("popup-content");
   popup.appendChild(popupContent);
   const footer = document.getElementById("footer");
-  document.body.insertBefore(popup,footer);
+  document.body.insertBefore(popup, footer);
   document.getElementById("popup").style.display = "flex";
 }
 
 //load data from cart
 document.addEventListener("DOMContentLoaded", () => {
-    const checkoutData = JSON.parse(localStorage.getItem('checkout_data'));
+  const checkoutData = JSON.parse(localStorage.getItem("checkout_data"));
 
-    if (checkoutData) {
-        document.querySelector('.order-product').textContent =
-            `${checkoutData.product} × ${checkoutData.quantity}`;
-        document.querySelector('.order-product-price').textContent =
-            `$${(checkoutData.price * checkoutData.quantity).toFixed(2)}`;
-        document.querySelectorAll('tbody tr:nth-child(2) td:last-child')[0].textContent =
-            `$${checkoutData.subtotal}`;
-        document.querySelector('.order-total').textContent =
-            `$${checkoutData.total}`;
-    }
+  if (checkoutData) {
+    document.querySelector(
+      ".order-product"
+    ).textContent = `${checkoutData.product} × ${checkoutData.quantity}`;
+    document.querySelector(".order-product-price").textContent = `$${(
+      checkoutData.price * checkoutData.quantity
+    ).toFixed(2)}`;
+    document.querySelectorAll(
+      "tbody tr:nth-child(2) td:last-child"
+    )[0].textContent = `$${checkoutData.subtotal}`;
+    document.querySelector(
+      ".order-total"
+    ).textContent = `$${checkoutData.total}`;
+  }
 });
