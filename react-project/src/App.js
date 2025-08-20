058d1f5206116470eed6b023d0b54cf27a352e7c
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-08-20 19:45:40 +0300

    added about page

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import SingleProduct from "./pages/singleProduct/SingleProduct";
+import About from "./pages/about/About";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/checkout" element={<Checkout checkoutData={{"product":"Denim Jacket","quantity":1,"price":"60.00","subtotal":"60.00","total":"60.00"}}/>} />
         <Route path="/single-product" element={<SingleProduct />} />
       </Routes>
---
+++ react-project/src/pages/about/About.js
@@ -0,0 +1,89 @@
+import './about.css';
+
+const About = () => {
+    return (
+        <div>
+            <header className="about-header">
+                <h1>ABOUT NORTHSTAR</h1>
+            </header>
+
+            <section className="container section-py">
+                <div className="hero-images-container">
+                    <div className="hero-image-card">
+                        <img src="../../assets/images/about1.png" alt="Woman Model" />
+                        <div className="buy-now-button-container">
+                            <button className="buy-now-button">BUY NOW</button>
+                        </div>
+                    </div>
+                    <div className="hero-image-card">
+                        <img src="../../assets/images/about2.png" alt="Man Model" />
+                        <div className="buy-now-button-container">
+                            <button className="buy-now-button">BUY NOW</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="container section-py founders-section">
+                <h2>The Founders</h2>
+                <div className="founders-grid">
+                    <div className="founder-card">
+                        <img src="../../assets/images/about4.png" alt="HM Jawwad" />
+                        <p>HM Jawwad</p>
+                    </div>
+                    <div className="founder-card">
+                        <img src="../../assets/images/about5.png" alt="Tufiqan Abid" />
+                        <p>Tufiqan Abid</p>
+                    </div>
+                    <div className="founder-card">
+                        <img src="/assets/images/about6.png" alt="Abdullah Ali" />
+                        <p>Abdullah Ali</p>
+                    </div>
+                    <div className="founder-card">
+                        <img src="../../assets/images/about7.png" alt="Akhtar Khan" />
+                        <p>Akhtar Khan</p>
+                    </div>
+                </div>
+            </section>
+
+            <section className="testimonials-section section-py">
+                <h2>Testimonials</h2>
+                <div className="testimonial-list">
+                    <div className="testimonial-item">
+                        <img src="../../assets/images/about8.png" alt="Stacy" />
+                        <div className="testimonial-content">
+                            <p className="testimonial-quote">
+                                We have ordered some accessories items and we got the products on
+                                time. Also the customer support department was super helpful and
+                                they answered all my queries.
+                            </p>
+                            <p className="testimonial-author">- Stacy</p>
+                        </div>
+                    </div>
+                    <div className="testimonial-item">
+                        <img src="../../assets/images/about9.png" alt="Tiffany" />
+                        <div className="testimonial-content">
+                            <p className="testimonial-quote">
+                                I ordered 5 shirts from them and received them in no time. The
+                                customer support was awesome.
+                            </p>
+                            <p className="testimonial-author">- Tiffany</p>
+                        </div>
+                    </div>
+                    <div className="testimonial-item">
+                        <img src="../../assets/images/about10.png" alt="James" />
+                        <div className="testimonial-content">
+                            <p className="testimonial-quote">
+                                I got a wrong shirt so I contacted them and they happily offered
+                                me a refund. I will surely shop from them again.
+                            </p>
+                            <p className="testimonial-author">- James</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default About;
