a680da945154fdfddcbbf6bc87af281408a83b71
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-08-20 19:40:29 +0300

    el vanilla baz

--- vanilla/scripts/navbarScript.js
+++ vanilla/scripts/navbarScript.js
@@ -1,4 +1,4 @@
-fetch("../components/navbar.html")
+fetch("../components/Navbar.html")
     .then((response) => response.text())
     .then((data) => {
         document.getElementById("navbar").innerHTML = data;
