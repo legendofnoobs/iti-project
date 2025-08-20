20c7101bf15b9b5ce96897330ea22db93e11c76b
moshams272 <mohamedshamseldeen95@gmail.com>
2025-08-20 22:47:41 +0300

    add store for App.js

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -5,19 +5,35 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import SingleProduct from "./pages/singleProduct/SingleProduct";
 import About from "./pages/about/About";
+import { DataProvider } from "./store/DataContext";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/checkout" element={<Checkout checkoutData={{"product":"Denim Jacket","quantity":1,"price":"60.00","subtotal":"60.00","total":"60.00"}}/>} />
-        <Route path="/single-product" element={<SingleProduct />} />
-      </Routes>
-      <Footer />
+      <DataProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/checkout"
+            element={
+              <Checkout
+                checkoutData={{
+                  product: "Denim Jacket",
+                  quantity: 1,
+                  price: "60.00",
+                  subtotal: "60.00",
+                  total: "60.00",
+                }}
+              />
+            }
+          />
+          <Route path="/single-product" element={<SingleProduct />} />
+        </Routes>
+        <Footer />
+      </DataProvider>
     </BrowserRouter>
   );
 }
